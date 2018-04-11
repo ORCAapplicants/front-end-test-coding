7c1652e86d38300b1702943df694670e06dfce92
fosiwaka2 <franciscocamachoguerra@gmail.com>
2018-04-11 03:27:59 -0500

    se agregó una validacion y las instrucciones

--- componentes/datos-usuario/modulo.js
+++ componentes/datos-usuario/modulo.js
@@ -2,10 +2,11 @@ angular
   .module('datosUsuario', ['ui.router'])
   .component('datosUsuario', {
     templateUrl: './componentes/datos-usuario/vista.html',
-    controller: ($scope, $stateParams) => {
+    controller: ($scope, $stateParams, $state) => {
       vm = $scope;
 
       vm.usuario = $stateParams.usuario;
+      !vm.usuario && $state.go('busqueda-usuarios');
     },
   })
   .name;
